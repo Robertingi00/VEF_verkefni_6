0c96a65b72c68a2d0cb1e06221b7890611af4790
Robertingi00 <robertha278@nemi.tskoli.is>
2018-02-28 16:54:21 +0000

    Update forrit_mv.py

--- forrit_mv.py
+++ forrit_mv.py
@@ -86,4 +86,5 @@ def error404(error):
 @error(500)
 def error500(error):
     return template('404error')
-run(host='localhost', port='8080', debug='True', reloader='True')
+run(host="0.0.0.0", port=os.environ.get('PORT'))
+
